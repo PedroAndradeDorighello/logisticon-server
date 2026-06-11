762623eca1cb01b80a2b19270256c1451fe42b52
PedroAndradeDorighello <pedro.dorighello@estudante.ufscar.br>
2026-06-11 17:41:37 -0300

    mais tempo de ping

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const db = admin.firestore();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    pingInterval: 3000, // Manda um "Ping" a cada 3 segundos
-    pingTimeout: 5000,   // Se não responder em 5s, considera que caiu
+    pingInterval: 25000, // Manda um "Ping" a cada 25 segundos
+    pingTimeout: 20000,   // Se não responder em 20s, considera que caiu
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
